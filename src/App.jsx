12ee88b643156e5ba268ff5de299fbbbc1d04661
Mahhen <nmahaboobmahen021@gmail.com>
2025-08-10 14:37:37 +0530

    wharra du

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavbarWithSimpleLinks from "./assets/components/navbar/Navbar";
+
+export default function App() {
+  return (
+    
+      <NavbarWithSimpleLinks />
+    
+  );
+}
