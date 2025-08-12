28106e2dd174fb2164b38e8c4bb4de52c4b253cd
Sujan Gaddam <sujansujan621311@gmail.com>
2025-08-12 16:20:11 +0530

    added backend folder with models

---
+++ backend/models/trains.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const trainSchema = new mongoose.Schema({
+  trn_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  trn_num: {
+    type: String,
+    required: true
+  },
+  trn_name: {
+    type: String,
+    required: true
+  },
+  trn_desc: {
+    type: String
+  },
+  trn_ticket: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  }
+})
+
+// Create model
+const Train = mongoose.model("Train", trainSchema)
+
+module.exports = Train
